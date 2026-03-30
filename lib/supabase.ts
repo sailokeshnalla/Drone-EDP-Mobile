1ed288fd463a77da272b3e48c045992d476098db
sailokeshnalla <135738733+sailokeshnalla@users.noreply.github.com>
2026-03-30 13:00:36 +0530

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vylkgnqdcnneonxcrquu.supabase.co";
-const supabaseAnonKey = "sb_publishable_9FcMN56X_24ubms6d_6WFA_VwBa4Dda";
+const supabaseUrl = "Project URL";
+const supabaseAnonKey = "Anon Key";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
